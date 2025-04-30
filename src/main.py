3de380d1929bf21ac410907733878ee2997ec3a3
LuisHenrique2000 <134005875+LuisHenrique2000@users.noreply.github.com>
2025-04-29 22:41:44 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 from flask import Flask
 from src.routes.public import public_bp
 from src.routes.admin import admin_bp
+from src.db import Base, engine  # <-- IMPORTANTE: conexão com banco
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(_name_)
     app.config["SECRET_KEY"] = "trocar-por-chave-segura"
 
     app.register_blueprint(public_bp)
     app.register_blueprint(admin_bp)
 
-    return app
+    Base.metadata.create_all(bind=engine)  # <-- CRIA AS TABELAS
+
+    return app
